ec1d4963b30f266c06bf1eb6483a0c0dfb1bab2b
mintkhwan <mskylinee@hotmail.com>
2016-04-24 14:30:44 +0700

    text

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
-
+app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000))
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -11,6 +12,20 @@ app.get('/webhook/', function (req, res) {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
+})
+
+app.post('/webhook/', function (req, res) {
+var  messaging_events = req.body.entry[0].messaging;
+  for (var i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    var sender = event.sender.id;
+    if (event.message && event.message.text) {
+    var text = event.message.text;
+      console.log(text);
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
 })
 
 app.listen(app.get('port'), function () {
