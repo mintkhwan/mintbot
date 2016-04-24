9f73f87315c60c5d3533525e7ea46628dd4e64f8
mintkhwan <mskylinee@hotmail.com>
2016-04-24 13:39:20 +0700

    webhook

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require('express')
 var app = express()
+
 app.set('port', (process.env.PORT || 5000))
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === '1234') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'))
 })
