d18ce8901142043bc415f9a7a93c3410109ab5df
mintkhwan <mskylinee@hotmail.com>
2016-04-24 14:35:49 +0700

    massage

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var request = require('request')
 var app = express()
+var text
+var token = 'CAAOCHbO8ZBuUBAESPFlZCGsg1w0uz7lTWrEALvOt8T0rtu1LlElBBZAUDDZAo3VPoAnXhfjFzRyoMdTAOCHYDH22RZCSyVRVaS3SP6cvmnIRZAW8ZAHrOmyhcylbDFKPIPd6YRUFPdaLXJM9o8oxdQJRt56AHZAip1xYt2PIKUxsYCzbYg9sAYhEkWzC8ddFuDDIgGsAMgFqNAZDZD'
+
 app.use(bodyParser.json())
-app.set('port', (process.env.PORT || 5000))
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === '1234') {
-    res.send(req.query['hub.challenge']);
+    res.send(req.query['hub.challenge'])
   }
-  res.send('Error, wrong validation token');
+  res.send('Error, wrong validation token')
 })
 
 app.post('/webhook/', function (req, res) {
-var  messaging_events = req.body.entry[0].messaging;
+  var messaging_events = req.body.entry[0].messaging
   for (var i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    var sender = event.sender.id;
+    var event = req.body.entry[0].messaging[i]
+    var sender = event.sender.id
     if (event.message && event.message.text) {
-    var text = event.message.text;
-      console.log(text);
+      text = event.message.text
       // Handle a text message from this sender
+      console.log(text)
+      if (text === 'Hi') {
+        sendTextMessage(sender, 'Hi! I am CoFen')
+      } else {
+        //sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
+      }
     }
   }
-  res.sendStatus(200);
+  res.sendStatus(200)
 })
 
+app.set('port', (process.env.PORT || 5000))
+
 app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'))
+  console.log('Example app listening on port ' + app.get('port') + '!')
 })
+
+function sendTextMessage (sender, text) {
+  var messageData = {
+    text: text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: messageData
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
